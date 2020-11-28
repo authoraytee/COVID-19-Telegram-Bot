632443f358284b78311edcfa1ba436f10561147b
authoraytee <authoraytee@mail.ru>
2020-11-28 12:44:53 +0700

    no new

--- config.py
+++ config.py
@@ -3,9 +3,8 @@ import os
 API_LINK = 'https://disease.sh/v3/covid-19/'
 API_GLOBAL_LINK = 'https://covid-stat.com/'
 
-#TOKEN = str(os.getenv('TOKEN'))
-TOKEN='1400859260:AAH0trEPMmX7zp0qrfpm4pRib8Ituq6E4uI'
-
+TOKEN = str(os.getenv('TOKEN'))
+#TOKEN='1400859260:AAH0trEPMmX7zp0qrfpm4pRib8Ituq6E4uI'
 
 admins = [
     466337120,
