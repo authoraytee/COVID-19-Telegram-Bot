241d7acf3eb2d0436593157fe8d8b4d4bcb53bb0
authoraytee <authoraytee@mail.ru>
2021-01-09 15:16:09 +0700

    ааа

--- config.py
+++ config.py
@@ -1,32 +1,24 @@
 
 #Data from 'secret' file, that I didn't load to github by obvious reason
-from secret import Token, DB_Host, DB_User, DB_Password, DB_Name
+#from secret import Token, DB_Host, DB_User, DB_Password, DB_Name
 
 API_LINK = 'https://disease.sh/v3/covid-19/'
 API_GLOBAL_LINK = 'https://covid-stat.com/'
 
-<<<<<<< HEAD
 #TOKEN = str(os.getenv('TOKEN'))
 TOKEN='1400859260:AAH0trEPMmX7zp0qrfpm4pRib8Ituq6E4uI'
-=======
-TOKEN=Token
->>>>>>> a4d81c3ef825b5489af11553893d267ee28c5cf3
 
 admins = [
     466337120,
 ]
 
-<<<<<<< HEAD
 DB_HOST= 'localhost'
 DB_USER='root'
 DB_PASSWORD='password1122'
 DB_NAME='covid_bot'
-=======
-DB_HOST = DB_Host
-DB_USER = DB_User
-DB_PASSWORD = DB_Password                     
-DB_NAME = DB_Name
 
+#DB_HOST = DB_Host
+#DB_USER = DB_User
+#DB_PASSWORD = DB_Password                     
+#DB_NAME = DB_Name
 
-
->>>>>>> a4d81c3ef825b5489af11553893d267ee28c5cf3
