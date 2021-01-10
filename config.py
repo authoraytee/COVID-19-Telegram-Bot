4a19f73bc51b8129943d5bfb5be58c5195056c1b
authoraytee <authoraytee@mail.ru>
2021-01-11 06:36:34 +0700

    ...

--- config.py
+++ config.py
@@ -1,24 +1,18 @@
 
-#Data from 'secret' file, that I didn't load to github by obvious reason
-#from secret import Token, DB_Host, DB_User, DB_Password, DB_Name
-
+# Sources from where we get the data (do not change)
 API_LINK = 'https://disease.sh/v3/covid-19/'
 API_GLOBAL_LINK = 'https://covid-stat.com/'
 
-#TOKEN = str(os.getenv('TOKEN'))
-TOKEN='1400859260:AAH0trEPMmX7zp0qrfpm4pRib8Ituq6E4uI'
+#Your bot token
+TOKEN='...'
 
+# The first administrator is you, enter your user ID (as a integer)
 admins = [
-    466337120,
+    000000000,
 ]
 
+# Database settings 
 DB_HOST= 'localhost'
 DB_USER='root'
 DB_PASSWORD='password1122'
 DB_NAME='covid_bot'
-
-#DB_HOST = DB_Host
-#DB_USER = DB_User
-#DB_PASSWORD = DB_Password                     
-#DB_NAME = DB_Name
-
