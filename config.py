4584c9a9b5d8562c8ea8d0a101f418474af9e27a
authoraytee <authoraytee@mail.ru>
2021-01-16 16:44:33 +0700

    a bit new

--- config.py
+++ config.py
@@ -1,15 +1,9 @@
-
 # Sources from where we get the data (do not change)
 API_LINK = 'https://disease.sh/v3/covid-19/'
 API_GLOBAL_LINK = 'https://covid-stat.com/'
 
 #Your bot token
 TOKEN='...'
-
-# The first administrator is you, enter your user ID (as an integer)
-admins = [
-    000000000,
-]
 
 # Database settings 
 DB_HOST= 'localhost'
