fca6a8434f93dac87a9bc2aed63a6f0ffe1e1806
authoraytee <authoraytee@mail.ru>
2021-01-16 16:44:56 +0700

    new faq, now it works (with db)

--- handlers/functions/faq.py
+++ handlers/functions/faq.py
@@ -1,35 +1,31 @@
 from aiogram import types
 from loader import dp, storage, bot
 
-from utils.database import connect
+from utils.database import get_answer, get_questions
 
-@dp.message_handler(commands='faq')
+from aiogram.types import CallbackQuery
+
+
+@dp.message_handler(commands=['faq'])
 @dp.callback_query_handler(text='faq')
-async def start_cmd_handler(message: types.Message):
+async def faq(query: types.CallbackQuery):
 
-    keyboard_markup = types.InlineKeyboardMarkup(row_width=1)
-    keyboard_markup.add(
-        types.InlineKeyboardButton('1. What is COVID-19?', callback_data='rer1'),
-        types.InlineKeyboardButton('2. What are the symptoms of COVID-19?', callback_data='rer2'),
-        types.InlineKeyboardButton('3. What happens to people who get COVID-19?', callback_data='rer3'),
-        types.InlineKeyboardButton('4. Who is most at risk of severe illness from COVID-19?', callback_data='rer4'),
-    )
-    text = '----- Frequently Asked Questions -----'
-    await message.reply(text, reply_markup=keyboard_markup)
+    text = [get_questions()]
+    text = '\n'.join(text)
+
+    await bot.send_message(query.from_user.id, text)
 
 
-@dp.callback_query_handler(text='rer1')
+
+commands = []
+for i in range(15):
+    commands.append('FAQ_{}'.format(i+1))
+
+@dp.message_handler(commands=commands)
 async def inline_kb_answer_callback_handler(query: types.CallbackQuery):
-    await bot.send_message(query.from_user.id, connect(1))
 
-@dp.callback_query_handler(text='rer2')
-async def inline_kb_answer_callback_handler(query: types.CallbackQuery):
-    await bot.send_message(query.from_user.id, connect(2))
+    question_number = query.text[-2::]
+    if question_number[0] == "_":
+        question_number = command_number[-1]
 
-@dp.callback_query_handler(text='rer3')
-async def inline_kb_answer_callback_handler(query: types.CallbackQuery):
-    await bot.send_message(query.from_user.id, connect(3))
-
-@dp.callback_query_handler(text='rer4')
-async def inline_kb_answer_callback_handler(query: types.CallbackQuery):
-    await bot.send_message(query.from_user.id, connect(4))
+    await bot.send_message(query.from_user.id, get_answer(question_number))
