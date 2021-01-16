83fb4aaa1feb19a7a304b21d31a7996d90e84636
authoraytee <authoraytee@mail.ru>
2021-01-16 16:45:41 +0700

    refactored some code and new features

--- utils/database.py
+++ utils/database.py
@@ -1,15 +1,48 @@
 import mysql.connector
 from mysql.connector import Error
 
-def connect(question_id):
+def connect():
+    conn = mysql.connector.connect(host='localhost',database='covid_bot',user='root',password='password1122')
+    if conn.is_connected():
+        print('Connected to MySQL database')
+    else: 
+        print('Connection to database failed, try later')
+    cursor = conn.cursor()
 
-    try:
-        conn = mysql.connector.connect(host='localhost',database='covid_bot',user='root',password='password1122')
-        if conn.is_connected():
-            print('Connected to MySQL database')
+    return conn
         
 
-        text = ['']
+def get_questions():
+    try:
+        conn = connect()
+
+        text = []
+        cursor = conn.cursor()
+        
+        for i in range(15):
+            cursor.execute("SELECT Question FROM qanda WHERE Id={};".format(i+1))
+            row = cursor.fetchone()
+            text.append('/FAQ_{} '.format(i+1) + row[0])
+
+            text.append('')
+
+        text = '\n'.join(text)
+
+        return text  
+
+    except Error as e:
+        print(e)
+
+    finally:
+        conn.close()
+
+
+def get_answer(question_id):
+
+    try:
+        conn = connect()
+
+        text = []
         cursor = conn.cursor()
         cursor.execute("SELECT Question FROM qanda WHERE Id={};".format(question_id))
         row = cursor.fetchone()
@@ -24,6 +57,7 @@ def connect(question_id):
 
         text = '\n'.join(text)
 
+
         return text  
 
     except Error as e:
@@ -31,7 +65,4 @@ def connect(question_id):
 
     finally:
         conn.close()
-                  
-
-
 
