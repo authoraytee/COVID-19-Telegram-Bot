d801fd9330fb29c3e0d06a15b68c8eab868cead5
authoraytee <authoraytee@mail.ru>
2021-01-18 03:39:09 +0700

    ss

--- config.py
+++ config.py
@@ -1,14 +1,12 @@
-# Используемое 
+# Sources from where we get the data (do not change)
 API_LINK = 'https://disease.sh/v3/covid-19/'
 API_GLOBAL_LINK = 'https://covid-stat.com/'
 
-TOKEN='1591382760:AAFx64bPcdiD9mLTZqCcSDQLSatvV8Sow-8'
+#Your bot token
+TOKEN='...'
 
-admins = [
-    466337120,
-]
-
-DB_HOST= 'us-cdbr-east-03.cleardb.com'
-DB_USER='bad1cd4ecce7d7'
-DB_PASSWORD='7ddb6879'
-DB_NAME='heroku_06573936f919916'
+# Database settings 
+DB_HOST= 'localhost'
+DB_USER='root'
+DB_PASSWORD='password1122'
+DB_NAME='covid_bot'
