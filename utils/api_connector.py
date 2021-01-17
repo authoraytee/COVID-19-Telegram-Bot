9d3f4103390050e088ce455d58dd236c65dabef6
authoraytee <authoraytee@mail.ru>
2021-01-18 02:09:36 +0700

    a bit new

--- utils/api_connector.py
+++ utils/api_connector.py
@@ -12,11 +12,16 @@ def get_response_global(param):
 def top(how_much, param):
     response = requests.get(API_LINK+'countries?sort='+param).json()
 
+    total_cases = []
+
     for i in range(how_much):
         message = response[i]
         country = message['country']
         parameter = message[param]
-        print(str(i + 1) + ": " + country + ' - ' + str(parameter) + " " + param)
+        get_country = str(i + 1) + ": " + country + ' - ' + str(parameter) + " " + param
+        total_cases.append(get_country)
+
+    return total_cases
 
 
 def my_country(country):
