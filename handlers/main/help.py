927acffab0213c0f34866873d761738300019953
authoraytee <authoraytee@mail.ru>
2021-01-17 22:13:10 +0700

    f

--- handlers/main/help.py
+++ handlers/main/help.py
@@ -25,5 +25,6 @@ async def inline_kb_answer_callback_handler(query: types.CallbackQuery):
     ]
     text = '\n'.join(text)
 
+    await bot.delete_message(message.chat.id, message.from_user.id)
     await bot.send_message(query.from_user.id, text)
 
